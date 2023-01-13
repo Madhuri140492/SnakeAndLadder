8dc27e413885fd544455338f1575b6f0aa3c6a19
Madhuri140492 <madhurideshmukh1404@gmail.com>
2023-01-13 14:12:15 +0530

    [Added : Player Checks for an option]

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,14 +1,37 @@
-package SnakeAndLadder;
-/*The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the
-number between 1 to 6*/
-public class RollsTheDie {
-    public static void main(String[] args) {
-        RollsTheDie a = new RollsTheDie();
-        a.rollDie();
+/*The Player then checks for a Option. They are No Play,
+Ladder or Snake. - Use ((RANDOM)) to check for Options - In Case of No Play the player stays in the same
+position
+- In Case of Ladder the player moves ahead by the number of position received in the die
+- In Case of Snake the player moves behind*/
 
-    }
-    public void rollDie(){
-        int die = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println("Die value is " + die);
+package SnakeAndLadder;
+public class CheckOption {
+    public static void main(String[] args) {
+        int position = 0;
+
+        int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println("DiceNumber is : " + dice);
+
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+        System.out.print("Player option is : ");
+
+        switch (option) {
+            case 0:
+                position += dice;
+                System.out.println("Ladder");
+                System.out.println("Position of the player after the ladder is : " + position);
+                break;
+
+            case 1:
+                position -=  dice;
+                System.out.println("Snake");
+                System.out.println("Position of the player after the Snake is : " + position);
+                break;
+
+            case 2:
+                System.out.println("No play");
+                System.out.println("Position of the player remains same");
+                break;
+        }
     }
 }
