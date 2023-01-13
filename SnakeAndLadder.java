f3c6a4eae436ec81eb2b780d22988f58ba145cdf
Madhuri140492 <madhurideshmukh1404@gmail.com>
2023-01-13 14:18:37 +0530

    [Added : player gets to exact winning position 100]

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,38 +1,43 @@
-/*Repeat till the Player reaches the winning position 100.
- - Note In case the player position moves below 0, then the player restarts from 0*/
+/*Ensure the player gets to exact winning position 100. - Note in case the player position go above
+100, the player stays in the same previous position till the player gets the exact
+number that adds to 100 */
 
 package com.bridgelabz.com;
 
-public class WinningPosition {
+public class ExactPosition {
     public static void main(String[] args) {
+        System.out.println("Player Exact Winning Position is: ");
 
-        System.out.println("Player initial Position is: ");
-
+        int EXACT_POSITION = 100;
         int position = 0;
 
-        while (position < 100) {
+        while (position < EXACT_POSITION) {
+
 
             int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-            System.out.println("diceNumber is : " + dice);
+            System.out.println("DiceNumber is : " + dice);
 
-            int option = (int) Math.floor(Math.random() * 10) % 3;
-            System.out.println("selected player option : " + option);
+            int option = (int) Math.floor(Math.random() * 10) % 2;
+            System.out.println("Selected player option : " + option);
 
             switch (option) {
                 case 0:
-                    position += dice;
+                    position += dice ;
                     System.out.println("Ladder");
-                    System.out.println("position for the player after the ladder is : " + position);
+                    if (position > 100) {
+                        position -= dice;
+                    }
+                    System.out.println("Position for the player after the ladder is : " + position);
                     break;
 
                 case 1:
-                    position -=  dice;
+                    position -= dice;
                     System.out.println("Snake");
                     if (position < 0) {
                         position = 0;
                         System.out.println("Player restart from zero: ");
                     }
-                    System.out.println("position for the player after the Snake is : " + position);
+                    System.out.println("Position for the player after the Snake is : " + position);
                     break;
 
                 case 2:
